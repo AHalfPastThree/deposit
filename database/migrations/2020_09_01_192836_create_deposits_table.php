c12a6ec059514ed3a52f756861752a09abe4540c
AHalfPastThree <rudenskyi.egor@gmail.com>
2020-09-01 23:18:55 +0300

    Created migrations

---
+++ database/migrations/2020_09_01_192836_create_deposits_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepositsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('wallet_id')->unsigned();
+            $table->foreign('wallet_id')->references('id')->on('wallets')->onDelete('cascade');
+            $table->double('invested', 0);
+            $table->double('percent', 0);
+            $table->smallInteger('active', 0);
+            $table->smallInteger('duration', 0);
+            $table->smallInteger('accrue_times', 0);
+            $table->timestamp('created_at', 0); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deposits');
+    }
+}
---
+++ database/migrations/2020_09_01_192926_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 30);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('wallet_id')->unsigned();
+            $table->foreign('wallet_id')->references('id')->on('wallets')->onDelete('cascade');
+            $table->integer('deposit_id')->nullable()->unsigned();
+            $table->foreign('deposit_id')->references('id')->on('deposits')->onDelete('cascade');
+            $table->double('amount', 0);
+            $table->timestamp('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
